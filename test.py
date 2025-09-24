403250010351f940e84fdb5b6e697337801d7283
김준회(mrt1847) <mrt1847@gepplus.net>
2025-09-24 12:13:08 +0900

    Update test.py

--- test.py
+++ test.py
@@ -6,23 +6,20 @@ from pom.Etc import Etc
 #pipenv run pytest --cache-clear test.py
 
 
-def test_srp(page):
+def test_srp_1(page):
     etc = Etc(page)
     srp_page = Srp(page)
     vip_page = Vip(page)
-    try:
-        etc.goto()
-        etc.login("cease2504", "asdf12!@")
+    etc.goto()
+    etc.login("cease2504", "asdf12!@")
 
-        srp_page.search_product("무선 이어폰")
-        vip_page.select_first_product()
-        vip_page.click_buy_now()
+    srp_page.search_product("무선 이어폰")
+    vip_page.select_first_product()
+    vip_page.click_buy_now()
 
 
-    except Exception as e:
-        raise e  # 테스트를 실패로 처리
-    
-def test(page):
+
+def test_srp_2(page):
     etc = Etc(page)
     srp_page = Srp(page)
     vip_page = Vip(page)
