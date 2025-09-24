e1c8bf075f52fa87634e305fe5666fff8a9b439e
김준회(mrt1847) <mrt1847@gepplus.net>
2025-09-24 16:44:12 +0900

    Update SrpPage.py

--- pom/SrpPage.py
+++ pom/SrpPage.py
@@ -1,3 +1,6 @@
+from playwright.sync_api import Page, expect
+
+
 class Srp():
     def __init__(self, page):
         self.page = page
@@ -5,3 +8,14 @@ class Srp():
     def search_product(self, keyword: str):
         self.page.fill("input[name='keyword']", keyword)
         self.page.press("input[name='keyword']", "Enter")
+
+    def before_search(self):
+
+        child = self.page.get_by_text("먼저 둘러보세요")
+        parent = child.locator("xpath=..")
+        expect(parent).to_be_visible()
+
+    def before_search_item(self):
+        child = self.page.get_by_text("먼저 둘러보세요")
+        parent = child.locator("xpath=../..")
+        expect(parent).to_be_visible()
